5dad9a91e7efa1d99d94cdb6b671d3d707201de5
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2025-10-25 14:32:51 +0200

    add loading screen

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
 
 const LoadingScreen = () => {
-  return (
-    <div>LoadingScreen</div>
-  )
-}
+    return (
+        <div className="min-h-screen bg-linear-to-br from-blue-50 to-indigo-300 flex items-center justify-center">
+             <p className="text-gray-600">Завантаження...</p>
+        </div>
+    );
+};
 
-export default LoadingScreen
+export default LoadingScreen;
